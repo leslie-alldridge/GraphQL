e13d6e9b36a46f76c28fa6f528b0a596310572cb
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-12 05:52:05 +1300

    data available in browser

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
